6f5444eb1fe6d3b57e8ea50c7716b01091424c2c
José Gabriel Abreu Moreira <83605515+JGabriel-AbreuM@users.noreply.github.com>
2021-11-07 20:10:09 -0300

    Update views.py

--- Ecommerece/ecommerce/produto/views.py
+++ Ecommerece/ecommerce/produto/views.py
@@ -1,13 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
-from django.db.models import F
-from django.shortcuts import render
+from django.db.models import F, fields
+from django.shortcuts import get_object_or_404, render, resolve_url
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView, View
 from .models import Categoria, Produto
 from django.urls import reverse_lazy
 from .forms import InsereCategoria, InsereProduto
 from django.contrib.auth.mixins import LoginRequiredMixin
+from user.models import CustomUser
+from .filters import ProdutoFilter
+from django_filters.views import FilterView
 
-class HomePageView(ListView):
+class HomePageView(FilterView):
     """
     Página home do projeto, quando o usuário estiver nela, verá todos os produtos então cadastrados
 
@@ -16,6 +20,7 @@ class HomePageView(ListView):
     model = Produto
     template_name = "home.html"
     context_object_name = "produtos"
+    filterset_class = ProdutoFilter
 
 
 class AddCategoriaView(LoginRequiredMixin, CreateView):
@@ -71,9 +76,56 @@ class DetailProdutoView(LoginRequiredMixin, DetailView):
     template_name = "compra.html"
     context_object_name = "produto"
 
-class ConfirmProdutoView(LoginRequiredMixin, UpdateView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context["preco_liquido"] = Produto.preco_liquido(self.object.id)
+
+        tipo = CustomUser.type_is(CustomUser.id_is(User.objects.all()[0])) == 'CP'
+
+        context["tipo"]  = tipo
+        return context
+
+
+class ConfirmProdutoView(LoginRequiredMixin, DetailView):
     """
     Confirma a compra de um determinado produto
     """
     model = Produto
     template_name = "confirma.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if CustomUser.type_is(CustomUser.id_is(User.objects.all()[0])) == 'CP':
+            Produto.atualiza_estoque(self.object.id)
+            context["invalido"] = False
+
+        else:
+            context["invalido"] = True
+        
+        return context
+
+class CarrinhoView(View):
+    def get(self, request, *args, **kwargs):
+        context = {}
+        response = render(request, 'carrinho.html', context)
+
+        ids = request.COOKIES["produtos"]
+        elements = []
+        for id in ids:
+            if id == ',':
+                continue
+
+            elements.append(int(id))
+
+        products = []
+        for e in elements:
+            products.append(Produto.return_produto(e))
+        
+        context["elementos"] = products    
+        response = render(request, 'carrinho.html', context)
+        return response
+
+class ConfirmCarrinhoView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'confirma_carrinho.html')

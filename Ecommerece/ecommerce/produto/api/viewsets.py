68936461b18c35a2a5d6090127bdda4437df3a90
José Gabriel Abreu Moreira <83605515+JGabriel-AbreuM@users.noreply.github.com>
2021-11-07 20:05:03 -0300

    Update viewsets.py

--- Ecommerece/ecommerce/produto/api/viewsets.py
+++ Ecommerece/ecommerce/produto/api/viewsets.py
@@ -1,9 +1,18 @@
+from django import http
 from rest_framework.viewsets import ModelViewSet
-from rest_framework import permissions
+from rest_framework import permissions, generics, serializers
+from rest_framework.response import Response
+from rest_framework.authtoken.serializers import AuthTokenSerializer
 from produto.models import Produto
 from .serializers import ProdutoSerializer
+from django.contrib.auth import login
+from django.contrib.auth.models import User
 
 class ProdutoViewSet(ModelViewSet):
+    """
+    ViewSet de minha API, apenas usuarios adminstradores terão acesso a esse
+    """
     queryset = Produto.objects.all()
     serializer_class = ProdutoSerializer
-    permission_classes = (permissions.IsAdminUser)
+    permission_classes = [permissions.IsAdminUser]
+
